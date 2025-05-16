48eb490b0d9275c0667613af6ad28862ef57ada3
Kavindu Anjana De Silva <kavinduanjana35@gmail.com>
2025-05-16 10:29:35 +0530

    Create navbar

---
+++ table-tennis-site/src/components/NavbarSection.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav class="navbar">
+    <div class="logo">🏓 Table Tennis Academy</div>
+    <ul class="nav-links">
+      <li><a href="#hero">Home</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#rules">Rules</a></li>
+      <li><a href="#contact">Contact</a></li>
+    </ul>
+  </nav>
+</template>
